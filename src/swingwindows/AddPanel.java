ef807f5bc866a94601c4b567f168dcc4398ed96b
NavaneethMv <mvnavaneeth5@gmail.com>
2022-12-29 15:58:28 +0530

    Minor changes

--- src/swingwindows/AddPanel.java
+++ src/swingwindows/AddPanel.java
@@ -2,6 +2,7 @@ package swingwindows;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.RoundRectangle2D;
 import javax.swing.border.*;
 
 public class AddPanel extends JPanel{
@@ -12,7 +13,100 @@ public class AddPanel extends JPanel{
         setVisible(true);
 
         GridBagLayout grid = new GridBagLayout();
-        setLayout(null);
+        GridBagConstraints gbc=new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 250, 5, 250);
 
+        setLayout(grid);
+
+        regNo = new JLabel("Reg. No.");
+        name = new JLabel("Name");
+        mark1 = new JLabel("mark1");
+        mark2 = new JLabel("mark2");
+        mark3 = new JLabel("mark3");
+        regNoTextField = new JTextField();
+        nameTextField = new JTextField();
+        mark1TextField = new JTextField();
+        mark2TextField = new JTextField();
+        mark3TextField = new JTextField();
+        submit = new JButton("SUBMIT");
+
+
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        regNo.setFont(new Font("Verdana", Font.PLAIN, 18));
+        add(regNo, gbc);
+        gbc.gridwidth = 2;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        regNoTextField.setFont(new Font("Verdana", Font.PLAIN, 18));
+        regNoTextField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.decode("#5A8F7B")));
+        add(regNoTextField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        name.setFont(new Font("Verdana", Font.PLAIN, 18));
+        add(name, gbc);
+        gbc.weightx = 2;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        nameTextField.setFont(new Font("Verdana", Font.PLAIN, 18));
+        nameTextField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.decode("#5A8F7B")));
+        add(nameTextField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        mark1.setFont(new Font("Verdana", Font.PLAIN, 18));
+        add(mark1, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        mark1TextField.setFont(new Font("Verdana", Font.PLAIN, 18));
+        mark1TextField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.decode("#5A8F7B")));
+        add(mark1TextField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        mark2.setFont(new Font("Verdana", Font.PLAIN, 18));
+        add(mark2, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        mark2TextField.setFont(new Font("Verdana", Font.PLAIN, 18));
+        mark2TextField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.decode("#5A8F7B")));
+        add(mark2TextField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        mark3.setFont(new Font("Verdana", Font.PLAIN, 18));
+        add(mark3, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 9;
+        mark3TextField.setFont(new Font("Verdana", Font.PLAIN, 18));
+        mark3TextField.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.decode("#5A8F7B")));
+        add(mark3TextField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 10;
+        add(submit, gbc);
+        submit.setFont(new Font("Verdana", Font.PLAIN, 18));
+        submit.setBorderPainted(false);
+        submit.setOpaque(true);
+        submit.setContentAreaFilled(true);
+        submit.setBackground(Color.decode("#5A8F7B"));
+        submit.setForeground(Color.white);
+        submit.setFocusPainted(false);
+        setBackground(Color.white);
     }
+    JLabel regNo;
+    JTextField regNoTextField;
+    JLabel name;
+    JTextField nameTextField;
+    JLabel mark1;
+    JTextField mark1TextField;
+    JLabel mark2;
+    JTextField mark2TextField;
+    JLabel mark3;
+    JTextField mark3TextField;
+    JButton submit;
+
+
+
+
+
+
 }
